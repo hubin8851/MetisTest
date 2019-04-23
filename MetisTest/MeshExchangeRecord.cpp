f9cf39fd906563a2d98d8f2ba4ba69cb1822f219
hubinxing <376898978@qq.com>
2019-04-23 17:56:32 +0800

    需要提交新的计时器，新的输出方式和简化部分检验步骤

--- MetisTest/MeshExchangeRecord.cpp
+++ MetisTest/MeshExchangeRecord.cpp
@@ -22,6 +22,17 @@ CGraphDepart::~CGraphDepart()
 
 mesh_t* CGraphDepart::ReadMesh(HBXFEMDef::InputRecord * _param)
 {
+	
+
+
+	return nullptr;
+}
+
+
+
+
+mesh_t * DynamicRecordToMesh(HBXFEMDef::InputRecord * _param)
+{
 	using namespace std;
 	using namespace HBXDef;
 	typedef std::map< std::string, std::shared_ptr< HBXFEMDef::NSortMat<HBXDef::UserReadPrec>> > _ElementData;
@@ -37,25 +48,17 @@ mesh_t* CGraphDepart::ReadMesh(HBXFEMDef::InputRecord * _param)
 	CheckUserDefErrors(_param->GiveField(_Element, HBXFEMDef::InputRecord::DataType_t::ALL_ELEMENT));
 	_param->GiveField(_Node, "Node");
 	_ElementData::iterator _iter;
-	while (_iter != ((_ElementData*)_Element)->end() )
+	while (_iter != ((_ElementData*)_Element)->end())
 	{
 		_mesh->ne = _iter->second->GetRowNum();
 		eptr = _mesh->eptr = ismalloc(_mesh->ne + 1, 0, "ReadMesh: eptr");
 		eind = _mesh->eind = imalloc(ntokens, "ReadMesh: eind");
-		ewgt = _mesh->ewgt = ismalloc((ncon == 0 ? 1 : ncon)*mesh->ne, 1, "ReadMesh: ewgt");
+		ewgt = _mesh->ewgt = ismalloc((ncon == 0 ? 1 : ncon)*_mesh->ne, 1, "ReadMesh: ewgt");
 	}
-	
-
 
 	return nullptr;
 }
 
 
-
-
-mesh_t * DynamicRecordToMesh(HBXFEMDef::InputRecord * _param)
-{
-	return nullptr;
-}
 
 }
